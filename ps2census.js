87e780e6279b9b72d0722121cfdc77fb7c2267ae
Ean McLaughlin <ean.mclaughlin@rocky.edu>
2016-04-28 21:30:33 -0600

    Added options-passing.

--- ps2census.js
+++ ps2census.js
@@ -2,14 +2,21 @@
  * Created by Pepper on 11/4/2015.
  */
 
-var defaultConfig = require('./config/default');
-var config = require('config');
+var config = require('./config/default');
 var cQuery = require('./lib/CensusQuery.js');
 var jQuery = require('./lib/JoinQuery.js');
 
-var PS2Census = function(options){
-    config.util.extendDeep(defaultConfig, options);
-    config.util.setModuleDefaults('census', defaultConfig);
+var PS2Census = function (options) {
+    if (options) {
+        for (option in config) {
+            if (!options[option])
+                options[option] = config[option];
+        }
+    }
+    else
+        options = config;
+
+    cQuery.prototype.options = options;
 };
 
 PS2Census.prototype.CensusQuery = cQuery;
